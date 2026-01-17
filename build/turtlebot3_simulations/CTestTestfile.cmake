09c0228b7e1ac54353566ba9a5aa0af007a3c0f7
Ali Suleiman <ali.slaiman496@gmail.com>
2026-01-17 23:13:55 +0300

    Initial commit for first_slam_project

---
+++ build/turtlebot3_simulations/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ali-suleiman/first_slam_project/src/turtlebot3_simulations
+# Build directory: /home/ali-suleiman/first_slam_project/build/turtlebot3_simulations
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ali-suleiman/first_slam_project/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/ali-suleiman/first_slam_project/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
